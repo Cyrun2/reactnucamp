871f513566103931a1d82322db88023bdae80355
Spencer Douglas <cyrun2@gmail.com>
2021-09-11 13:00:44 -0400

    Workshop 4 Assignment

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal,
+ModalHeader, ModalBody, Row, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
 
 function RenderCampsite({campsite}) {
     return (
@@ -13,6 +19,93 @@ function RenderCampsite({campsite}) {
             </Card>
         </div>
     );
+}
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false,
+            touched: {
+                author: false
+            } 
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleComment = this.handleComment.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleComment(values) {
+        alert(`Rating: ${values.rating} Your Name: ${values.author} Comment: 
+        ${values.text}`);
+        this.toggleModal();
+        console.log(values);
+    }
+
+    render() {
+        return(
+            <>
+                <Button outline className='fa-lg' onClick={this.toggleModal}>
+                    <i className='fa fa-pencil' />Submit Comment
+                </Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                        <ModalBody>
+                            <LocalForm onSubmit={this.handleComment}>
+                                <div className="form-group">
+                                    <Label htmlFor="Rating">Rating</Label>
+                                    <Control.select model='.rating' id='rating' name='rating' 
+                                    className='form-control'>                                
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </Control.select>
+                                </div>
+                                <div className="form-group">
+                                    <Label htmlFor="Your Name">Your Name</Label>    
+                                    <Control.text model='.author' id='author' name='author' 
+                                    className='form-control' 
+                                    validators={{
+                                        minLength: minLength(2),
+                                        maxLength: maxLength(15)
+                                    }}/>
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <Label htmlFor="Comment">Comment</Label>
+                                    <Control.textarea model='.text' id='text' name='text'
+                                    className='form-control'>
+                                    </Control.textarea>
+                                </div>
+                                <Button type="submit" value="submit" color="primary">
+                                    Submit</Button>
+                            </LocalForm>
+                        </ModalBody> 
+                </Modal>
+            </>
+        )  
+    }
+        
+        
 }
 
 function RenderComments({comments}) {
@@ -32,6 +125,7 @@ function RenderComments({comments}) {
                         );
                     })
                 }
+                <CommentForm />   
             </div>
         );
     }
--- nucampsite/src/components/ContactComponent.js
+++ nucampsite/src/components/ContactComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem,
     Button, Label, Col, Row } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Link } from 'react-router-dom';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -9,7 +10,7 @@ const minLength = len => val => val && (val.length >= len);
 const isNumber = val => !isNaN(+val);
 const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
-import { Link } from 'react-router-dom';
+
 
 class Contact extends Component {
     constructor(props) {
